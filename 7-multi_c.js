2418b0d13732bb68c976b8321a931bbf3116bd0a
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-05-22 17:40:54 +0200

    c is cool

---
+++ 7-multi_c.js
@@ -0,0 +1,16 @@
+const {argv} = require('node:process');
+
+
+if (argv[2] == undefined){
+    console.log("Missing number of occurrences")
+}
+
+else if (isNaN(parseInt(argv[2]))){
+    console.log("Missing number of occurrences")
+}
+
+else {
+   for (let i = 0; i < argv[2]; i++){
+        console.log("C is fun")
+   }
+}

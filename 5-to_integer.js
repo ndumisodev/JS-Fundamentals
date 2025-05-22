9621c95818069b471abef1b756ae0e34240411cd
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-05-22 17:20:15 +0200

    c is cool

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -3,9 +3,20 @@ const {argv} = require('node:process');
 if (argv[2] == undefined){
     console.log("Not a number")
 }
-else if (typeof(argv[2]) == Number){
+else if (isNaN(parseInt(argv[2]))) {
+    console.log("Not a number");
+  }
+else if (typeof(parseInt(argv[2])) == "number" ){
     console.log(`My number: ${Math.floor(argv[2])}`)
 }
+// else if (typeof(argv[2]) == Number){
+// }
+// else if (typeof(parseInt(argv[2])) == Number){
+//     console.log(`My number: ${parseInt(argv[2])}`)
+// }
 else{
-    console.log(`My number: ${parseInt(argv[2])}`)
+    console.log("Not a number")
 }
+
+
+// console.log(typeof(parseInt(argv[2])))

63a5ebd424bd75781a3299805cd41a3d0a18df15
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-05-22 17:28:31 +0200

    c is cool

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -9,11 +9,7 @@ else if (isNaN(parseInt(argv[2]))) {
 else if (typeof(parseInt(argv[2])) == "number" ){
     console.log(`My number: ${Math.floor(argv[2])}`)
 }
-// else if (typeof(argv[2]) == Number){
-// }
-// else if (typeof(parseInt(argv[2])) == Number){
-//     console.log(`My number: ${parseInt(argv[2])}`)
-// }
+
 else{
     console.log("Not a number")
 }
---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+const langArray = ["C is fun", "Python is cool", "JavaScript is amazing" ]
+
+for (lang of langArray){
+    console.log(lang)
+}
